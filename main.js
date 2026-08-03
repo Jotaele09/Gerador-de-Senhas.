0891fcc8e3bdf1e90b9f7215558b77d93545ed29
Jotaele09 <araujo.joao15@escola.pr.gov.br>
2026-08-03 11:53:01 -0300

    f

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.parametro-senha_botao')
+const numeroSenha = documento.querySelector (´.parametro-senha_texto´);
+numeroSenha.textCOntent = 5;
+const botoes = document.querySelectorA11  ('.parametro-senha botao');
+console.log (botoes);
+ functon diminuiTamanho() {
+
+
+    
+ }
